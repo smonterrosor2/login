6455ab1060b9e930663f2b8fc219ef27cec52948
Katherine Gonzalez <katygonzo99@gmail.com>
2024-04-02 21:32:23 -0600

    MantenimientoProductos2

--- login/obj/Debug/net8.0-windows/login.AssemblyInfo.cs
+++ login/obj/Debug/net8.0-windows/login.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31b697560a7c766fffe4843422b8f93cf9bc8038")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+210850b1441ef5bfbe0c51d3ab0f5fe9ee1c0c8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
